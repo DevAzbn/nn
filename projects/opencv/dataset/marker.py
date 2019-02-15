60cc3242c0bcac44ed52c0e917629f23f24fce35
azbn <i@azbn.ru>
2019-02-15 13:43:10 +0300

    201902151343

--- projects/opencv/dataset/marker.py
+++ projects/opencv/dataset/marker.py
@@ -17,11 +17,12 @@ drawing = False
 image, image_, image__, rimage = None, None, None, None
 project = ''
 actfile = ''
+dpath = ''
 result = {}
 
 
 def draw_shape(event, x, y, flags, param):
-	global x0, y0, x1, y1, drawing, image, image_, image__, rimage, actfile, result
+	global x0, y0, x1, y1, drawing, image, image_, image__, rimage, actfile, result, dpath
 
 	alpha = 0.5
 	
@@ -60,7 +61,7 @@ def draw_shape(event, x, y, flags, param):
 			rimage = image_
 			result[actfile].append([(x0, y0), (x1, y1)]) #, (x1 - x0, y1 - y0)
 			#cv2.imwrite("{}_{}_{}_{}_{}.jpg".format(actfile, x0, y0, x1, y1), image[y0:y1, x0:x1])
-			cv2.imwrite("cache/{}_{}.jpg".format(actfile, len(result[actfile])), image[y0:y1, x0:x1])
+			cv2.imwrite(dpath + "/cache/{}_{}.jpg".format(actfile, len(result[actfile])), image[y0:y1, x0:x1])
 		# drawing = False
 		# if mode == True:
 		# 	cv2.rectangle(image, (ix, iy), (x, y), (0, 255, 0), -1)
@@ -68,16 +69,16 @@ def draw_shape(event, x, y, flags, param):
 		# 	cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
 
 def main(d):
-	global x0, y0, x1, y1, drawing, image, image_, image__, rimage, actfile, result
+	global x0, y0, x1, y1, drawing, image, image_, image__, rimage, actfile, result, dpath
 
 	prefix = './projects/'
 	dpath = prefix + d
-	files = os.listdir(dpath)
+	files = os.listdir(dpath + '/pos')
 	files = filter(lambda x: x.endswith('.jpg') or x.endswith('.png'), files)
 
 	for f in files:
 		actfile = f
-		filename = dpath + '/' + f
+		filename = dpath + '/pos/' + f
 		print(filename)
 		result[f] = []
 
@@ -107,8 +108,14 @@ def main(d):
 			# 	# elif k == 20:
 			# 	# 	pass
 			# 	# if k == ord('m') or k == ord('M'):
-		with open(dpath + '/marked.json', 'w') as f:
-			json.dump(result, f, ensure_ascii = False)
+		with open(dpath + '/marked.json', 'w') as fi:
+			json.dump(result, fi, ensure_ascii = False)
+		str = ''
+		for l in result:
+			for i in result[l]:
+				str = str + "./{} 1 {} {} {} {}".format(d + '/' + f, i[0][0], i[0][1], i[1][0], i[1][1]) + "\n"
+		with open(dpath + '/pos.txt', 'w') as fi:
+			fi.write(str)
 		cv2.destroyAllWindows()
 
 
