8db907b7a71addbed7aeb73787f2c3472d0dcb60
azbn <i@azbn.ru>
2019-02-27 08:40:51 +0300

    201902270840

--- projects/other/mysql/app.py
+++ projects/other/mysql/app.py
@@ -8,23 +8,31 @@ import MySQLdb
 # sudo apt-get install python-dev default-libmysqlclient-dev
 # sudo apt-get install python3-dev
 
-conn = MySQLdb.connect(host = '192.168.0.34', port = 3306, user = 'office_fedoperat', passwd = 'office_fedoperat', db = 'office_fedoperat', use_unicode = True, charset = 'utf8')
-cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+try:
+	conn = MySQLdb.connect(host = '192.168.0.34', port = 3306, user = 'office_fedoperat', passwd = 'office_fedoperat', db = 'office_fedoperat', use_unicode = True, charset = 'utf8')
+	cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+except MySQLdb.Error as err:
+	print("Connection error: {}".format(err))
 
-cursor.execute("""SELECT * FROM `external_ad` WHERE 1 ORDER BY `id` LIMIT %s""", (3,))
-# .executemany
-# .query
+try:
+	
+	cursor.execute("""SELECT * FROM `external_ad` WHERE 1 ORDER BY RAND() LIMIT %s""", (3,))
+	# .executemany
+	# .query
 
-#rows = cursor.fetchmany(5)
-rows = cursor.fetchall()
-for item in rows:
-	print(item['title'])
+	#rows = cursor.fetchmany(5)
+	rows = cursor.fetchall()
+	for item in rows:
+		print(item['title'])
 
-# cursor.execute("SELECT * FROM `external_ad` WHERE 1 ORDER BY `id` LIMIT 20")
+	# cursor.execute("SELECT * FROM `external_ad` WHERE 1 ORDER BY `id` LIMIT 20")
 
-# # Получаем данные.
-# row = cursor.fetchone()
-# print(row)
+	# # Получаем данные.
+	# row = cursor.fetchone()
+	# print(row)
+
+except MySQLdb.Error as err:
+	print("Query error: {}".format(err))
 
 # Разрываем подключение.
 conn.close()
