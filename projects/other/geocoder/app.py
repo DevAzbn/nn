894c1f89da71c5d7b4dad80541845d36da6bd355
azbn <i@azbn.ru>
2019-06-24 08:33:27 +0300

    201906240833

--- projects/other/geocoder/app.py
+++ projects/other/geocoder/app.py
@@ -1,10 +1,29 @@
-import geocoder
-res = geocoder.yandex('Орел, Грановского 2')
-print(res.latlng)
-print(res.city)
-print(res.json)
+import sys
+import json
 
-# g = geocoder.ip('199.7.157.0')
-# g = geocoder.ip('me')
-# print(g.latlng)
-# print(g.city)
+import geocoder
+
+if __name__ == '__main__':
+
+	args = sys.argv
+
+	params = ['--text=']
+
+	for a in args:
+		if a.startswith(params[0]):
+			text = a[len(params[0]):]
+			r = None
+			try:
+				res = geocoder.yandex(text)
+				r = res.latlng
+				r = (float(r[0]), float(r[1]))
+			except:
+				pass
+			print(json.dumps(r))
+			# print(res.city)
+			# print(res.json)
+
+			# g = geocoder.ip('199.7.157.0')
+			# g = geocoder.ip('me')
+			# print(g.latlng)
+			# print(g.city)
